46e731ca2d3ce8adfce5fcada0f33ef52f2ff68c
tuuzed <liuyonghui.job@gmail.com>
2021-01-30 15:59:06 +0800

    修复Bugs

--- gradle-plugin/src/main/java/base64file/gradle/plugin/Base64File.java
+++ gradle-plugin/src/main/java/base64file/gradle/plugin/Base64File.java
@@ -1,42 +1,35 @@
 package base64file.gradle.plugin;
 
 import base64file.Base64FileUtils;
-import org.codehaus.groovy.runtime.InvokerHelper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Base64File {
 
+    public String from(File file) throws IOException {
+        return from(file, false);
+    }
+
     public String from(File file, boolean zip) throws IOException {
         String[] array = Base64FileUtils.encode(file, zip);
-        List<String> list = new ArrayList<>(array.length);
+        return from(array);
+    }
+
+    private String from(String[] array) {
         StringBuilder sb = new StringBuilder();
-        sb.append("new java.lang.String[]{\n\"");
-        sb.append(join(list, "\",\n\""));
-        sb.append("\"\n}");
+        sb.append("new java.lang.String[]{");
+        boolean first = true;
+        for (String it : array) {
+            if (!first) {
+                sb.append(",");
+            }
+            sb.append("\"");
+            sb.append(it);
+            sb.append("\"");
+            first = false;
+        }
+        sb.append("}");
         return sb.toString();
     }
-
-    private static String join(Iterable self, String separator) {
-        StringBuilder buffer = new StringBuilder();
-        boolean first = true;
-        if (separator == null) {
-            separator = "";
-        }
-        Object value;
-        for (Iterator var4 = self.iterator(); var4.hasNext(); buffer.append(InvokerHelper.toString(value))) {
-            value = var4.next();
-            if (first) {
-                first = false;
-            } else {
-                buffer.append(separator);
-            }
-        }
-        return buffer.toString();
-    }
-
 }
